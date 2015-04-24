4fa6a5e676ef17d33f389f65a8ae56b7432407e4
Michael Knowles <mknowles@fulcrum.net>
2015-04-24 13:21:07 -0700

    Cleanup indenting.

--- uplift.py
+++ uplift.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 
 class UpliftDesk:
-    delay = 0.2
+    delay = 0.2 # how long to "hold" the button down when simulating an up/down press
     pinup = 18
     pindown = 22
     def __init__(self, pinup, pindown):
@@ -11,8 +11,8 @@ class UpliftDesk:
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self.pinup, GPIO.OUT) 
         GPIO.setup(self.pindown, GPIO.OUT) 
-		GPIO.output(self.pinup,True)
-		GPIO.output(self.pindown,True)
+        GPIO.output(self.pinup,True)
+        GPIO.output(self.pindown,True)
     def up(self):
         GPIO.output(self.pinup, False)
         time.sleep(self.delay)
@@ -20,25 +20,22 @@ class UpliftDesk:
     def down(self):
         GPIO.output(self.pindown, False)
         time.sleep(self.delay)
-        GPIO.output(self.pindown,True)        
+        GPIO.output(self.pindown,True)
 
 uplift = UpliftDesk(18,22)
 
 ##Define a function named Blink()
 def Blink(numTimes,speed):
-	for i in range(0,numTimes):## Run loop numTimes
-		print "Iteration " + str(i+1)## Print current loop
+    for i in range(0,numTimes):## Run loop numTimes
+        print "Iteration " + str(i+1)## Print current loop
         uplift.up()
-		time.sleep(speed)## Wait
+        time.sleep(speed)## Wait
         uplift.down()
-		time.sleep(speed)## Wait
-	print "Done" ## When loop is complete, print "Done"
+        time.sleep(speed)## Wait
+    print "Done" ## When loop is complete, print "Done"
 
-## Ask user for total number of blinks and length of each blink
-iterations = raw_input("Enter total number of times to blink: ")
-speed = raw_input("Enter length of each blink(seconds): ")
 
 ## Start Blink() function. Convert user input from strings to numeric data types and pass to Blink() as parameters
-Blink(int(iterations),float(speed))
+Blink(10,10)
 
 GPIO.cleanup()
