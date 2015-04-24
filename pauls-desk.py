5716e8fd54c51bdbb3fe402d074721157a66992d
Michael Knowles <mknowles@fulcrum.net>
2015-04-24 15:23:53 -0700

    Clean up comments

--- pauls-desk.py
+++ pauls-desk.py
@@ -2,18 +2,16 @@ import uplift
 
 desk = uplift.UpliftDesk(18,22)
 
-##Define a function named Blink()
 def Blink(numTimes,speed):
-    for i in range(0,numTimes):## Run loop numTimes
-#        print "Iteration " + str(i+1)## Print current loop
+    for i in range(0,numTimes):
+#        print "Iteration " + str(i+1)
         desk.up()
-        time.sleep(speed)## Wait
+        time.sleep(speed)
         desk.down()
-        time.sleep(speed)## Wait
-#    print "Done" ## When loop is complete, print "Done"
+        time.sleep(speed)
+#    print "Done"
 
 
-## Start Blink() function. Convert user input from strings to numeric data types and pass to Blink() as parameters
 Blink(10,10)
 
 GPIO.cleanup()
