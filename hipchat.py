d082bc89615744616c03c94275c0418c4a754bf5
W. S. Wellington <bill@wellingtonnet.net>
2015-04-24 19:52:56 -0700

    normalizing line spacing
    Signed-off-by: W. S. Wellington <bill@wellingtonnet.net>

--- hipchat.py
+++ hipchat.py
@@ -41,66 +41,53 @@ auth = 'Bearer ' + token
 
 
 def _api_get(url, data=None):
-    url, data = url, data
-    
+    url, data = url, data    
     requisite_headers = { 'Accept' : 'application/json',
                           'Content-Type' : 'application/json',
                           'Authorization' : auth
     }
-    
     response =  requests.get(url, headers=requisite_headers)
-    
     return response.status_code, response.text
 
 
 def _api_put(url, data):
     url, name, passwd = url, user, token
-    
-    
     requisite_headers = { 'Accept' : 'application/json',
                           'Content-Type' : 'application/json'
     }
-    auth = (name, passwd) 
- 
+    auth = (name, passwd)
 #     if len(argv) > 3:
 #         data = load_file(argv[3])
 #     else:
 #         data = None
-
     response =  requests.put(url, headers=requisite_headers, auth=auth, params=data)
-
     return response.status_code, response.text
-    
+
 
 def _api_post(url, data):
     url, data = url, data
-    
     requisite_headers = { 'Accept' : 'application/json',
                           'Content-Type' : 'application/json',
                           'Authorization' : auth
-    }
- 
+    } 
     response =  requests.post(url, headers=requisite_headers, data=data)
-    
     return response.status_code, response.text
 
 
 def _api_del(argv):
     url, name, passwd = argv[0], argv[1], argv[2]
-    
     requisite_headers = { 'Accept' : 'application/json',
                           'Content-Type' : 'application/json'
     }
-    auth = (name, passwd) 
-    
+    auth = (name, passwd)
     response =  requests.delete(url, headers=requisite_headers, auth=auth)
-    
     return response.status_code, response.text
 
 
 def rest_usage():
     print "usage: rest [put|get|post|delete] url name passwd"
     sys.exit(-1)
+
 
 cmds = {
     "GET": _api_get, 
@@ -109,34 +96,33 @@ cmds = {
     "DELETE": _api_del
     }
 
+
 def load_file(fname):
   with open(fname) as f:
     return f.read()
 
-def rest(req, url, data=None):
 
+def rest(req, url, data=None):
 #     if len(argv) < 4:
 #         rest_usage()
     print url
-    
     if 'HTTPS' not in url.upper():
         print "Secure connection required: HTTP not valid, please use HTTPS or https"
-        rest_usage()       
-        
+        rest_usage()
     cmd = req.upper()
     if cmd not in cmds.keys():
         rest_usage()
-
     status,body=cmds[cmd](url, data)
     print
     if int(status) == 200:
         json_output = json.loads(body)
-        print json.dumps(json_output, indent = 4)        
+        print json.dumps(json_output, indent = 4)
         return body
     else:
         print "Status: %s\n%s" % (status, body)
         print
-        
+
+
 ############################################################################ 
 #### begin custom functions
 
@@ -144,23 +130,21 @@ def list_rooms():
   url = base_url+'/v2/room'
   body = rest('get', url, data=None)
   json_output = json.loads(body)
-  
   l = []
 #   for j in json_output:
 #     l.append(j.get('id'))
-    
   return l
+
 
 def list_room_participants(room_id):
   url = base_url+'/v2/room/'+room_id+'/participant'
   body = rest('get', url, data=None)
   json_output = json.loads(body)
-  
   l = []
 #   for j in json_output:
 #     l.append(j.get('items'))
-    
   return l
+
 
 def message_room(room_id):
   data = json.dumps({
@@ -170,42 +154,29 @@ def message_room(room_id):
         'notify' : False})
   url = base_url+'/v2/room/'+room_id+'/message'
   rest('post', url, data)
-  
 
 
-
-
-
-
-#       
-# 
 # def get_exclusions():
 #   exclusions = []
 #   f = open(control_dir+'/exclusions-final.conf', 'r')
 #   for line in f:
-#     exclusions.append(line.split("#",1)[0].rstrip())
-#     
+#    exclusions.append(line.split("#",1)[0].rstrip())
 #   exclusions = filter(None, exclusions)  
-#   
 #   #encode exclusions in unicode
 #   unicode_exclusions = [unicode(i) for i in exclusions]
-#   
 #   return unicode_exclusions
-#   
-# 
+
+
 # def suspend_configurations():
 #   configurations = set(get_configurations())
 #   exclusions = set(get_exclusions())
 #   suspends = list(configurations - exclusions)
-#   
 #   data = {'runstate' : 'suspended'}
-# 
 #   for i in suspends:
 #     print i
 #     rest('put', base_url+'/configurations/2156312?runstate=suspended', user, token, data=data)
 
 
-  
 ############################################################################ 
 #### begin interface items
 
@@ -215,9 +186,9 @@ def usage(exitcode):
   usage="""
   
   
-##########################    Welcome to Skynet    #########################
+##########################    Welcome to Hipchat API    #########################
 
-  skynet [--help] [--action=<action-name>]
+  hipchat [--help] [--action=<action-name>]
 
 OPTIONS:
   --help, -h
@@ -227,16 +198,15 @@ OPTIONS:
     Take an action
 
 ACTIONS:
-  'suspend' will suspend configurations that are not on the exclusions list.
   
 EXAMPLES:
-  skynet -a suspend 
 
 ############################################################################ 
 
 
-  
+
 """
+
 
   try:
     exitcode
@@ -247,34 +217,28 @@ EXAMPLES:
     print usage
     sys.exit(exitcode)
 
- 
+
 # argument parser
 def ui(argv):
-  
 # define variables to be used by getopts and sent them to null
   action = ''
   scope = ''
-  
-#  print 'ARGV     :', sys.argv[1:]
-  
+# print 'ARGV     :', sys.argv[1:]
   try:
     options, remainder = getopt.gnu_getopt(sys.argv[1:], 'a:hs:t', ['help',
                                                           'action=',
                                                           'scope=',
                                                           ])
-                                            
   except getopt.GetoptError:
     usage()
     sys.exit(2)
 
 
 #  print 'OPTIONS    :', options
-
   for opt, arg in options:
     if opt in ('-h', '--help'):
       usage(2)
       sys.exit()
-
     elif opt in ( '-a', '--action' ):
       action = arg
       if action == 'list-rooms':
@@ -285,22 +249,17 @@ def ui(argv):
         message_room('1452085')
       elif action != 'suspend':
         usage(2)
-      
     elif opt in ( '-s', '--scope' ):
       scope = arg
       usage(3)
-
     elif opt in ( '-t' ):
       print 'TEST ENVIRONMENT'
-    
-#   print 'ACTION   :', action
-#   print 'SCOPE      :', scope
-#   print 'REMAINING  :', remainder
+#     print 'ACTION   :', action
+#     print 'SCOPE      :', scope
+#     print 'REMAINING  :', remainder
 
 
 
-
- 
 # call main
 if __name__=='__main__':
 
